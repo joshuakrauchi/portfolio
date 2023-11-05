b08e1ae38353384e854644d429e07dcba630f8f5
Joshua Krauchi <joshuakrauchi@gmail.com>
2023-11-04 22:43:40 -0700

    Fixed implicit any usage.

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const ContactButton = ({ imageSource, animationDelay, size, address }) => {
+const ContactButton = (props: { imageSource: any, animationDelay: any, size: any, address: any }) => {
   return (
     <>
-      <Link href={address} className="animate__animated animate__fadeInRight flex justify-center w-16 h-16 rounded-full p-2 bg-white hover:bg-blue-500" style={{animationDelay: animationDelay + "ms"}}>
-        <Image width={size} height={size} src={imageSource} alt={address}></Image>
+      <Link href={props.address} className="animate__animated animate__fadeInRight flex justify-center w-16 h-16 rounded-full p-2 bg-white hover:bg-blue-500" style={{animationDelay: props.animationDelay + "ms"}}>
+        <Image width={props.size} height={props.size} src={props.imageSource} alt={props.address}></Image>
       </Link>
     </>
   );
--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
-const MenuButton = ({ buttonText, address, onClick, isActive }) => {
+const MenuButton = (props: { buttonText: string, address: string, onClick: any, isActive: boolean }) => {
   return (
     <>
-      <Link href={address} onClick={onClick} className={"text-center text-white hover:bg-blue-500 py-4 w-48 text-xl " + (isActive ? "bg-blue-700" : "bg-black")}>
-        { buttonText }
+      <Link href={props.address} onClick={props.onClick} className={"text-center text-white hover:bg-blue-500 py-4 w-48 text-xl " + (props.isActive ? "bg-blue-700" : "bg-black")}>
+        { props.buttonText }
       </Link>
     </>
   );
